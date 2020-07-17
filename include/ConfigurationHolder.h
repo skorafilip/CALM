2f6c0964da233a105e52b4c86aac0490ff11862d
Filip Skóra <skorafilip@gmail.com>
2020-07-17 13:53:48 +0200

    Another parameters and ini array reader implemented

--- include/ConfigurationHolder.h
+++ include/ConfigurationHolder.h
@@ -8,6 +8,7 @@
 //FILIPS: nowa klasa, ma na celu przechowywanie parametrów wczytanych w plikach konfiguracyjnych
 //zakładam że gdy będą dochodzić nowe paramtery trzeba będzie dodawać nowe zmienne
 //wszystko wczytuje się raz na początku działania programu, potem jest tylko odczyt z pól klasy, wydaje mi się, że to optymalne rozwiązanie
+using namespace std;
 class ConfigurationHolder {
 	
 	public:
@@ -15,9 +16,27 @@ class ConfigurationHolder {
         ConfigurationHolder(Configurator *sMainConfig);
 
         int pythiaMult;
-        std::string singleEnergyDistr;
+        string pionsMultDistr;
+        int pionsMultDistr_xMin;
+        int pionsMultDistr_xMax;
+        string kaonsMultDistr;
+        int kaonsMultDistr_xMin;
+        int kaonsMultDistr_xMax;
+        string nucleonsMultDistr;
+        int nucleonsMultDistr_xMin;
+        int nucleonsMultDistr_xMax;
+        string lambdasMultDistr;
+        int lambdasMultDistr_xMin;
+        int lambdasMultDistr_xMax;
+
+        double* Nmean;
+        double RapidityInterval;
+        double* XYZ;
+
+        string singleEnergyDistr;
         int MinEtot;
         double* divideEn;
+
         //int EventsPerFile;
 
 };
--- src/ConfigurationHolder.cxx
+++ src/ConfigurationHolder.cxx
@@ -3,11 +3,25 @@
 #include <TDatime.h>
 
 
-ConfigurationHolder::ConfigurationHolder()
+vector<string> SplitString(string strarray){
+  vector<string> vresult;
+  int index;
+  while((index = strarray.find(",")) != string::npos){
+    string strvalue = strarray.substr(0, index);
+    strarray.erase(0, index + 1);
+    vresult.push_back(strvalue);
+  }
+  vresult.push_back(strarray);
+
+  return vresult;
+}
+
+
+/*ConfigurationHolder::ConfigurationHolder()
 : pythiaMult(0), singleEnergyDistr("0.922477*(TMath::Power(x+2.15717,-1.57383)-1.40499e-05)"), 
   MinEtot(7000), divideEn(new double[2]{1,1})//, EventsPerFile(10000)
 {
-}
+}*/
 
 ConfigurationHolder::ConfigurationHolder(Configurator *sMainConfig){
     TDatime tDate;
@@ -16,17 +30,62 @@ ConfigurationHolder::ConfigurationHolder(Configurator *sMainConfig){
     PRINT_MESSAGE("["<<tDate.AsSQLString()<<"]\tLodaing configuration.");
 
     try{
+      string s = sMainConfig->GetParameter("Nmean").Data();
+      vector<string> vNmean = SplitString(sMainConfig->GetParameter("Nmean").Data());
+      if(vNmean.size()!=4){
+        throw *(new TString("Nmean"));
+      }
+      Nmean = new double[4]{
+          stod(vNmean[0]),
+          stod(vNmean[1]),
+          stod(vNmean[2]),
+          stod(vNmean[3])
+
+      };
+      RapidityInterval = sMainConfig->GetParameter("RapidityInterval").Atoi();
+      vector<string> vXYZ = SplitString(sMainConfig->GetParameter("XYZ").Data());
+      if(vNmean.size()!=3){
+        throw *(new TString("XYZ"));
+      }
+      XYZ = new double[3]{
+          stod(vXYZ[0]),
+          stod(vXYZ[1]),
+          stod(vXYZ[2])
+      };
+
       pythiaMult = sMainConfig->GetParameter("pythiaMult").Atoi();
+
+      pionsMultDistr = sMainConfig->GetParameter("pionsMultDistr");
+      pionsMultDistr_xMin = sMainConfig->GetParameter("pionsMultDistr_xMin").Atoi();
+      pionsMultDistr_xMax = sMainConfig->GetParameter("pionsMultDistr_xMax").Atoi();
+
+      kaonsMultDistr = sMainConfig->GetParameter("kaonsMultDistr");
+      kaonsMultDistr_xMin = sMainConfig->GetParameter("kaonsMultDistr_xMin").Atoi();
+      kaonsMultDistr_xMax = sMainConfig->GetParameter("kaonsMultDistr_xMax").Atoi();
+
+      nucleonsMultDistr = sMainConfig->GetParameter("nucleonsMultDistr");
+      nucleonsMultDistr_xMin = sMainConfig->GetParameter("nucleonsMultDistr_xMin").Atoi();
+      nucleonsMultDistr_xMax = sMainConfig->GetParameter("nucleonsMultDistr_xMax").Atoi();
+
+      lambdasMultDistr = sMainConfig->GetParameter("lambdasMultDistr");
+      lambdasMultDistr_xMin = sMainConfig->GetParameter("lambdasMultDistr_xMin").Atoi();
+      lambdasMultDistr_xMax = sMainConfig->GetParameter("lambdasMultDistr_xMax").Atoi();
+
+
       singleEnergyDistr = sMainConfig->GetParameter("singleEnergyDistr");
       MinEtot = sMainConfig->GetParameter("MinEtot").Atoi();
+      
+      vector<string> vdivideEn = SplitString(sMainConfig->GetParameter("divideEn").Data());
+      if(vNmean.size()!=2){
+        throw *(new TString("divideEn"));
+      }
       divideEn = new double[2]{
-          sMainConfig->GetParameter("divideEn_energyofparticles").Atoi(),
-          sMainConfig->GetParameter("divideEn_boostenergy").Atoi()
+          stoi(vdivideEn[0]),
+          stoi(vdivideEn[1])
       };
-      //EventsPerFile = sMainConfig->GetParameter("EventsPerFile").Atoi();
     }
     catch(TString s){
-      PRINT_MESSAGE("Lack of parameters ("<<s<<"), program aborted!");
+      PRINT_MESSAGE("Lack of parameters or invalid format ("<<s<<"), program aborted!");
       exit(_ERROR_CONFIG_PARAMETER_NOT_FOUND_);
     }
 
