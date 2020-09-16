9ec3923e1e3e6b2f389bb8d6c9474ee460a946be
Filip Skóra <skorafilip@gmail.com>
2020-09-16 19:29:52 +0200

    Backgroud simulation with script

--- Tools/run_CALM.sh
+++ Tools/run_CALM.sh
@@ -1,35 +1,40 @@
 #!/bin/bash
+#args: EventType, NumberOfEvents
 cp events.ini events.ini.bak
-NumberOfCores=4
+cores=4
 
 #0.setting amount of events to generate
-let amount=$2/$NumberOfCores
+let amount=$2/$cores
 sed -i "s,^EventType =.*$,EventType = $1," events.ini
 sed -i "s,^NumberOfEvents =.*$,NumberOfEvents = $amount," events.ini
 
-#1.making name and directiories for each process
+#1.creating root directory for simulation
 EventDir=$(awk -F "=" '/EventDir/ {print $2}' events.ini)
+#EventDir="events_EventType$1/"
 mkdir $EventDir
 
 
-for i in $( seq 1 $NumberOfCores )
+function CALM(){
+#2.making name and directiories for each process
+sleep ${1}s
+declare calm${1}dir="${EventDir}calm${1}/"
+eval "mkdir \${calm${1}dir}"
+
+#3.change directiory and run CALM
+eval "sed -i \"s,^EventDir =.*$,EventDir = \${calm${1}dir},\" events.ini"
+
+./calm
+}
+
+declare command="CALM 1"
+
+for i in $( seq 2 $cores )
 do
-declare calm${i}dir="${EventDir}calm${i}/"
-eval "mkdir \${calm${i}dir}"
+command="${command} & CALM $i"
 
 done
 
-
-#2.change directiory and run CALM
-for i in $( seq 1 $NumberOfCores )
-do
-
-eval "sed -i \"s,^EventDir =.*$,EventDir = \${calm${i}dir},\" events.ini"
-
-gnome-terminal -- ./calm
-sleep 1s
-
-done
+eval $command
 
 sleep 5s
 cp events.ini.bak events.ini
